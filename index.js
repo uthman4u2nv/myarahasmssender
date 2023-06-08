d5f3f96d4b8bbd01d325d6208d0cb9ebc9d6bb02
Ajagbe Uthman Olawale <uthman4u2nv@yahoo.com>
2023-06-08 23:58:35 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const nodeCron = require("node-cron");
+const winston = require('winston');
+require('dotenv').config();
+let fileName=new Date().getDay()+"-"+new Date().getMonth()+"-"+new  Date().getFullYear()+".log";
+const logConfiguration = {
+    
+    'transports': [
+        
+        //new winston.transports.Console()
+        new winston.transports.File({
+            filename: 'logs/'+fileName
+        })
+    ]
+};
+const logger = winston.createLogger(logConfiguration);
+var axios = require('axios');
+
+const smssendurl=process.env.SMSSendUrl;
+const apikey=process.env.APIKey;
+
+const job = nodeCron.schedule("15 * * * * *", async function jobYouNeedToExecute() {
+  // Do whatever you want in here. Send email, Make  database backup or download data.
+  await SendSMS("2348032518766","Testing the sms gateway")
+  console.log(new Date().toLocaleString());
+});
+
+const SendSMS=async (phone,sms)=>{
+    let body={
+        "api_key":apikey,
+        "to":phone,
+        "from":"MyAraha",
+        "sms":sms,
+        "type":"plain",
+        "channel":"dnd"
+    }
+    logger.info(new Date().toLocaleString()+JSON.stringify(body));
+      try{
+        var config = {
+            method: 'POST',
+            url:smssendurl,
+            headers: { 
+              'Content-Type': 'application/json',
+            },
+            data:body
+          };
+          axios(config)
+      .then(async function (response) {
+        console.log(JSON.stringify(response.data));
+        logger.info(JSON.stringify(response.data));
+        if(response.data){
+            let resp={
+                ResponseCode:"00",
+                ResponseMessage:response.message,
+                
+            }
+            console.log(resp)
+        }else{
+            let resp={
+                ResponseCode:"69",
+                ResponseMessage:response.data.message,
+                
+            }
+            console.log(resp);
+        }
+      })
+    }catch(err){
+        logger.error(err.message);
+        console.log(err.message)
+    }finally{
+        let resp={
+            ResponseCode: "69",
+            ResponseMessage:"Error sending message,please try again later"
+        }
+        console.log(resp);
+    }
+}
