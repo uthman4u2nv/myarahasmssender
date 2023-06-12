59ebb4308d56d4087d52891346a1f49c4dfeb97c
Ajagbe Uthman Olawale <uthman4u2nv@yahoo.com>
2023-06-12 21:33:47 +0100

    Second Commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const nodeCron = require("node-cron");
 const winston = require('winston');
+var conn=require('./connection');
 require('dotenv').config();
-let fileName=new Date().getDay()+"-"+new Date().getMonth()+"-"+new  Date().getFullYear()+".log";
+const { convertPhoneNumberToInternationFormat } = require('@toluwap/phone-number-formatter');
+
+
+//let fileName=new Date().getDay()+1+"-"+new Date().getMonth()+1+"-"+new  Date().getFullYear()+".log";
+let fileName=ReturnDate()+".log";
 const logConfiguration = {
     
     'transports': [
@@ -27,17 +32,149 @@ var axios = require('axios');
 const smssendurl=process.env.SMSSendUrl;
 const apikey=process.env.APIKey;
 
-const job = nodeCron.schedule("15 * * * * *", async function jobYouNeedToExecute() {
+const job = nodeCron.schedule("10 * * * * *", async function jobYouNeedToExecute() {
   // Do whatever you want in here. Send email, Make  database backup or download data.
-  await SendSMS("2348032518766","Testing the sms gateway")
+  let response=0;
+  let sms=[];
+  let phone=[];
+  let msg="";
+  let smsID=[];
+  
+  
+    let smsc=await ReturnSMS().then(function(value) {
+        sms=value;
+        (sms).forEach(async element => {
+           // const phoneNumbers1 = convertPhoneNumberToInternationFormat({phoneNumbers: element['smsTo'], options: { prepend: '+'}});
+            let result=await SendSMS(element['smsTo'],element['sms']).then(async function(val){
+                logger.info("")
+                console.log("Sending SMS Value:",val);
+                if(val=="Success"){
+                    console.log("Operation 1,"+val+"SMS ID:"+element['smsID']);
+                    
+                    let up=await UpdateSMSStatus(1,element['smsID']).then(function(vv){
+                        logger.info("SMS Successfully sent to:"+element['smsTo']);
+                    }).catch(function(vx){
+                        logger.error("Error Updating SMS Status:"+vx)
+                    })
+                }else{
+                    
+                    console.log("Operation 2,"+val+"SMS ID:"+element['smsID']);
+                    
+                    let up=await UpdateSMSStatus(1,element['smsID']).then(function(vvv){
+                        logger.info("SMS Successfully sent to:"+element['smsTo']);
+                    }).catch(function(vvx){
+                        logger.error("Error Updating SMS Status:"+vvx)
+                    })
+                }
+            }).catch(function(v){
+                console.log("Error Value:",v);
+            })
+        });
+    }).catch(function(err){
+        console.log("Error:",err.message);
+    });
+  
+  
+    /*SendSMSs(phone,sms);
+    (smsID).forEach(async element=>{
+        let u="";
+        let up=await UpdateSMSStatus(1,element['smsID']).then(function(value){
+            logger.info("SMS Successfully sent to:"+smsID.length+" Phone Number(s)");
+        }).catch(function(v){
+            logger.error("Error Updating SMS Status:"+v)
+        })
+    })*/
+        
+        /*let result=await SendSMS(element['smsTo'],element['sms']).then(async function(value){
+            response=value;
+            console.log("Value",value);
+            logger.info("Result from Sending SMS:"+response)
+            //if(response=1){
+                //update
+                let u="";
+                let up=await UpdateSMSStatus(1,element['smsID']).then(function(value){
+                    logger.info("SMS Successfully sent to:"+element['smsTo']);
+                }).catch(function(v){
+                    logger.error("Error Updating SMS Status:"+v)
+                })*/
+            /*}else{
+                let u="";
+                let up=await UpdateSMSStatus(3,element['smsID']).then(function(value){
+                    logger.error("Error Sending SMS to:"+element['smsTo']);
+                }).catch(function(v){
+                    
+                })
+            }*/
+       /* }).catch(function(v){
+
+        });
+    //});*/
+/*}).catch(function(v) {
+    allusers=v;
+  });*/
   console.log(new Date().toLocaleString());
 });
 
-const SendSMS=async (phone,sms)=>{
+
+const SendSMS=async(phone,sms)=>{
+    return new Promise(function(resolve,reject){
+        let body={
+            "api_key":apikey,
+            "to":phone,
+            "from":"N-Alert",
+            "sms":sms,
+            "type":"plain",
+            "channel":"dnd"
+        }
+        try{
+            var config = {
+                method: 'POST',
+                url:smssendurl,
+                headers: { 
+                  'Content-Type': 'application/json',
+                },
+                data:body
+              };
+              axios(config)
+          .then(async function (response) {
+            
+            logger.info(response.data);
+            logger.info(JSON.stringify(response.data.message));
+            if(response.data.message=="Successfully Sent"){
+                resolve("Success");
+            }else{
+                reject("Failed")
+            }
+            /*if(response.data.message==="Successfully Sent"){
+                let resp={
+                    ResponseCode:"00",
+                    ResponseMessage:response.data.message,
+                    
+                }
+                
+               
+            }else{
+                let resp={
+                    ResponseCode:"69",
+                    ResponseMessage:response.data.message,
+                    
+                }
+                reject(0);
+                
+            }*/
+          })
+        }catch(err){
+            console.log("SMS Sending Error:"+err.message);
+            logger.error("Error:"+err.message)
+reject("Failed")
+        }
+    })
+}
+const SendSMSs=async (phone,sms)=>{
     let body={
         "api_key":apikey,
         "to":phone,
-        "from":"MyAraha",
+        "from":"N-Alert",
         "sms":sms,
         "type":"plain",
         "channel":"dnd"
@@ -83,4 +220,77 @@ const SendSMS=async (phone,sms)=>{
         }
         console.log(resp);
     }
+}
+
+const ReturnSMS=async()=>{
+    var row=[];
+    return new Promise(function(resolve,reject){
+        var query="SELECT * FROM smsnew where DATE(smsDate) = CURDATE() AND smsStatus='0'";
+    conn.query(query,(err,rows)=>{
+            if(!err){
+                
+            //cnt=rows[0].cnt;
+            row=rows;
+                
+                resolve(row)
+                
+            }else{
+                cnt=0;
+                reject(row);
+            }
+        })
+        
+    })
+}
+
+const UpdateSMSStatus=async(status,smsID)=>{
+    console.log("Status:"+status);
+    console.log("SMS ID:"+smsID);
+    //var status=0;
+    return new Promise(function(resolve,reject){
+        var query="UPDATE smsnew SET smsStatus='"+status+"' WHERE smsID='"+smsID+"'";
+        //var query="UPDATE smsnew SET smsStatus='1' WHERE smsID='3'";
+    conn.query(query,(err,rows)=>{
+            if(!err){
+                
+            //cnt=rows[0].cnt;
+            status=1;
+                
+                resolve(1)
+                
+            }else{
+                logger.info("Error Updating Status with smsID:"+smsID+" Status:"+status);
+                console.log("Error Updating",err.message);
+                cnt=0;
+                reject(0);
+            }
+        })
+        
+    })
+}
+
+function ReturnDate(){
+    let date_time = new Date();
+
+// get current date
+// adjust 0 before single digit date
+let date = ("0" + date_time.getDate()).slice(-2);
+
+// get current month
+let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+
+// get current year
+let year = date_time.getFullYear();
+
+// get current hours
+let hours = date_time.getHours();
+
+// get current minutes
+let minutes = date_time.getMinutes();
+
+// get current seconds
+let seconds = date_time.getSeconds();
+
+// prints date in YYYY-MM-DD format
+return year + "-" + month + "-" + date;
 }
